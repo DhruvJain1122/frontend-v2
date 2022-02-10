2140c151e16871aee1615b38c9d43b7320ba721c
Tim Robinson <tim@timjrobinson.com>
2022-02-10 19:02:19 +1000

    Hacky first version loading from JSON file and showing table

--- src/pages/vebal.vue
+++ src/pages/vebal.vue
@@ -1,12 +1,45 @@
 <script setup lang="ts">
 import Hero from '@/components/contextual/pages/vebal/Hero.vue';
 
+import useGauges from '@/composables/vebal/useGauges';
+import useWeb3 from '@/services/web3/useWeb3';
+import usePoolFilters from '@/composables/pools/usePoolFilters';
+
 import { isVeBalSupported } from '@/composables/useVeBAL';
+import GaugesTable from '@/components/tables/GaugesTables/GaugesTable.vue';
+
+const { isWalletReady } = useWeb3();
+
+const {
+  selectedTokens,
+  addSelectedToken,
+  removeSelectedToken
+} = usePoolFilters();
+
+const {
+  gauges,
+  isLoadingGauges,
+} = useGauges(selectedTokens);
 </script>
 
 <template>
   <div v-if="isVeBalSupported" class="lg:container lg:mx-auto pt-10 md:pt-12">
     <Hero />
+    <template v-if="isWalletReady">
+      <div class="px-4 lg:px-0">
+        <BalStack horizontal justify="between" align="center">
+          <h3>Pools eligible for liquidity mining</h3>
+        </BalStack>
+      </div>
+      <GaugesTable
+        :isLoading="isLoadingGauges"
+        :data="gauges"
+        :noPoolsLabel="$t('noInvestments')"
+        showPoolShares
+        :selectedTokens="selectedTokens"
+        class="mb-8"
+      />
+    </template>
   </div>
   <div v-else class="text-center">
     <div class="font-semibold text-lg">
